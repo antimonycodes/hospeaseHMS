66ca56a0dc562e12f667ce8e4e6475c0db69460b
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2025-04-04 01:26:24 -0700

    fix: reorder imports in FrontdeskInfo component for consistency

--- src/components/Frontdesk/patients/FrontdeskInfo.tsx
+++ src/components/Frontdesk/patients/FrontdeskInfo.tsx
@@ -1,8 +1,9 @@
 import { JSX, useState } from "react";
-import Table from "../../../Shared/Table";
+
 import { useNavigate } from "react-router-dom";
 import Loader from "../../../Shared/Loader";
 import EditIcon from "../../../assets/EditIcon.png";
+import Table from "../../../Shared/Table";
 type FrondeskPatientData = {
   name: string;
   patientId: string;
